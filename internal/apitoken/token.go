7427b3434cca110a2cbf1a4e497c004dd9d3dd6c
soup <soup@r4tio.dev>
2023-02-20 09:56:31 +0000

    deny negative values

--- internal/apitoken/token.go
+++ internal/apitoken/token.go
@@ -3,13 +3,19 @@ package apitoken
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
 )
 
 func GenerateToken(length int) (string, error) {
-	b := make([]byte, length)
-	_, err := rand.Read(b)
-	if err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(b), nil
+    if length <= 0 {
+        return "", fmt.Errorf("token length must be a positive integer")
+    }
+
+    b := make([]byte, length)
+    _, err := rand.Read(b)
+    if err != nil {
+        return "", err
+    }
+
+    return hex.EncodeToString(b), nil
 }

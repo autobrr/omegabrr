f0e2f11dd625ad57ab8b15c5f2d50ec53e6fe0f0
soup <soup@r4tio.dev>
2023-02-23 21:42:46 +0100

    enhancement(titles): improved parsing (#37)
    * feat(titles): clean exclamation marks
    
    * enhancement(title): improved parsing
    
    * optimization
    
    * refactoring
    
    * refactor
    
    * comments
    
    * another apostrophe to strip
    
    * go mod tidy
    
    * improved and simplified
    
    * nuxens improvements + optimizing and comments
    
    * trim whitespaces from end of string
    
    * improved comment about empty title check
    
    * minor improvements to parentheses
    
    * refactored handling of parentheses
    
    * implemented a parsing test
    
    * Added more cases to title_test.go
    
    Revert handling of hyphens in original title.
    Will still be converted to a ? in new title.
    
    * Do not trim ? from title in parentheses case
    
    * trim commas in the first output
    
    * improved handling of parentheses
    
    added condition for parentheses in the middle of a title
    
    * enhancement(parsing): improved parsing of titles and added tests
    
    * enhancement(parsing): updated tests and deps
    
    * go mod tidy
    
    * fix(parsing): added one missing option
    
    * refactor: tests
    
    ---------
    
    Co-authored-by: nuxen <47067662+nuxencs@users.noreply.github.com>
    Co-authored-by: ze0s <zes0@riseup.net>

---
+++ internal/processor/title_test.go
@@ -0,0 +1,161 @@
+package processor
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_processTitle(t *testing.T) {
+	type args struct {
+		title        string
+		matchRelease bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "test_01",
+			args: args{
+				title:        "The Quick Brown Fox (2022)",
+				matchRelease: false,
+			},
+			want: []string{"The?Quick?Brown?Fox"},
+		},
+		{
+			name: "test_02",
+			args: args{
+				title:        "The Matrix     -        Reloaded (2929)",
+				matchRelease: false,
+			},
+			want: []string{"The?Matrix*Reloaded"},
+		},
+		{
+			name: "test_03",
+			args: args{
+				title:        "The Matrix -(Test)- Reloaded (2929)",
+				matchRelease: false,
+			},
+			want: []string{"The?Matrix*Test*Reloaded"},
+		},
+		{
+			name: "test_04",
+			args: args{
+				title:        "The Marvelous Mrs. Maisel",
+				matchRelease: false,
+			},
+			want: []string{"The?Marvelous?Mrs*Maisel"},
+		},
+		{
+			name: "test_05",
+			args: args{
+				title:        "Arrr!! The Title (2020)",
+				matchRelease: false,
+			},
+			want: []string{"Arrr*The?Title"},
+		},
+		{
+			name: "test_06",
+			args: args{
+				title:        "Whose Line Is It Anyway? (US)",
+				matchRelease: false,
+			},
+			want: []string{"Whose?Line?Is?It?Anyway", "Whose?Line?Is?It?Anyway*US", "Whose?Line?Is?It?Anyway?"},
+		},
+		{
+			name: "test_07",
+			args: args{
+				title:        "MasterChef (US)",
+				matchRelease: false,
+			},
+			want: []string{"MasterChef*US", "MasterChef"},
+		},
+		{
+			name: "test_08",
+			args: args{
+				title:        "Brooklyn Nine-Nine",
+				matchRelease: false,
+			},
+			want: []string{"Brooklyn?Nine?Nine"},
+		},
+		{
+			name: "test_09",
+			args: args{
+				title:        "S.W.A.T.",
+				matchRelease: false,
+			},
+			want: []string{"S?W?A?T?", "S?W?A?T"},
+		},
+		{
+			name: "test_10",
+			args: args{
+				title:        "The Handmaid's Tale",
+				matchRelease: false,
+			},
+			want: []string{"The?Handmaid?s?Tale", "The?Handmaids?Tale"},
+		},
+		{
+			name: "test_11",
+			args: args{
+				title:        "The Handmaid's Tale (US)",
+				matchRelease: false,
+			},
+			want: []string{"The?Handmaid?s?Tale*US", "The?Handmaids?Tale*US", "The?Handmaid?s?Tale", "The?Handmaids?Tale"},
+		},
+		{
+			name: "test_12",
+			args: args{
+				title:        "Monsters, Inc.",
+				matchRelease: false,
+			},
+			want: []string{"Monsters*Inc?", "Monsters*Inc"},
+		},
+		{
+			name: "test_13",
+			args: args{
+				title:        "Hello Tomorrow!",
+				matchRelease: false,
+			},
+			want: []string{"Hello?Tomorrow?", "Hello?Tomorrow"},
+		},
+		{
+			name: "test_14",
+			args: args{
+				title:        "Be Cool, Scooby-Doo!",
+				matchRelease: false,
+			},
+			want: []string{"Be?Cool*Scooby?Doo?", "Be?Cool*Scooby?Doo"},
+		},
+		{
+			name: "test_15",
+			args: args{
+				title:        "Scooby-Doo! Mystery Incorporated",
+				matchRelease: false,
+			},
+			want: []string{"Scooby?Doo*Mystery?Incorporated"},
+		},
+		{
+			name: "test_16",
+			args: args{
+				title:        "Master.Chef (US)",
+				matchRelease: false,
+			},
+			want: []string{"Master?Chef*US", "Master?Chef"},
+		},
+		{
+			name: "test_17",
+			args: args{
+				title:        "Whose Line Is It Anyway? (US)",
+				matchRelease: false,
+			},
+			want: []string{"Whose?Line?Is?It?Anyway*US", "Whose?Line?Is?It?Anyway?", "Whose?Line?Is?It?Anyway"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.ElementsMatch(t, tt.want, processTitle(tt.args.title, tt.args.matchRelease), tt.name)
+		})
+	}
+}

dc4e27a7c3c7df9b84e01fefdd33a52316d252a7
soup <soup@r4tio.dev>
2023-03-09 16:47:16 +0100

    added json support

--- internal/processor/regbrr.go
+++ internal/processor/regbrr.go
@@ -2,6 +2,7 @@ package processor
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -15,37 +16,78 @@ import (
 func (s Service) regbrr(ctx context.Context, cfg *domain.ArrConfig, dryRun bool, brr *autobrr.Client) error {
 	l := log.With().Str("type", "regbrr").Str("client", cfg.Name).Logger()
 
-	l.Debug().Msgf("fetching titles from URL: %s", cfg.Host)
+	var titles []string
 
-	resp, err := http.Get(cfg.Host)
-	if err != nil {
-		l.Error().Err(err).Msgf("failed to fetch titles from URL: %s", cfg.Host)
-		return err
-	}
-	defer resp.Body.Close()
+	if strings.HasSuffix(cfg.Host, ".json") {
+		l.Debug().Msgf("fetching titles from JSON URL: %s", cfg.Host)
 
-	if resp.StatusCode != http.StatusOK {
-		l.Error().Msgf("failed to fetch titles from URL: %s", cfg.Host)
-		return fmt.Errorf("failed to fetch titles from URL: %s", cfg.Host)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		l.Error().Err(err).Msgf("failed to read response body from URL: %s", cfg.Host)
-		return err
-	}
-
-	titleLines := strings.Split(string(body), "\n")
-	titles := []string{}
-	for _, titleLine := range titleLines {
-		title := strings.TrimSpace(titleLine)
-		if title == "" {
-			continue
+		resp, err := http.Get(cfg.Host)
+		if err != nil {
+			l.Error().Err(err).Msgf("failed to fetch titles from URL: %s", cfg.Host)
+			return err
 		}
-		titles = append(titles, title)
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			l.Error().Msgf("failed to fetch titles from URL: %s", cfg.Host)
+			return fmt.Errorf("failed to fetch titles from URL: %s", cfg.Host)
+		}
+
+		var data []struct {
+			Movie struct {
+				Title string `json:"title"`
+			} `json:"movie"`
+			Show struct {
+				Title string `json:"title"`
+			} `json:"show"`
+		}
+
+		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+			l.Error().Err(err).Msgf("failed to decode JSON data from URL: %s", cfg.Host)
+			return err
+		}
+
+		for _, item := range data {
+			if item.Movie.Title != "" {
+				titles = append(titles, item.Movie.Title)
+			}
+			if item.Show.Title != "" {
+				titles = append(titles, item.Show.Title)
+			}
+		}
+
+	} else {
+		l.Debug().Msgf("fetching titles from URL: %s", cfg.Host)
+
+		resp, err := http.Get(cfg.Host)
+		if err != nil {
+			l.Error().Err(err).Msgf("failed to fetch titles from URL: %s", cfg.Host)
+			return err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			l.Error().Msgf("failed to fetch titles from URL: %s", cfg.Host)
+			return fmt.Errorf("failed to fetch titles from URL: %s", cfg.Host)
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			l.Error().Err(err).Msgf("failed to read response body from URL: %s", cfg.Host)
+			return err
+		}
+
+		titleLines := strings.Split(string(body), "\n")
+		for _, titleLine := range titleLines {
+			title := strings.TrimSpace(titleLine)
+			if title == "" {
+				continue
+			}
+			titles = append(titles, title)
+		}
 	}
 
-	l.Debug().Msgf("gathered titles: %v", titles)
+	//l.Debug().Msgf("gathered titles: %s", strings.ReplaceAll(strings.Join(titles, ", "), ", ", ","))
 
 	for _, filterID := range cfg.Filters {
 		l.Debug().Msgf("updating filter: %v", filterID)
@@ -53,6 +95,14 @@ func (s Service) regbrr(ctx context.Context, cfg *domain.ArrConfig, dryRun bool,
 		filterTitles := []string{}
 		for _, title := range titles {
 			filterTitles = append(filterTitles, processTitle(title, cfg.MatchRelease)...)
+		}
+
+		joinedTitles := strings.Join(filterTitles, ",")
+
+		l.Trace().Msgf("%s", joinedTitles)
+
+		if len(joinedTitles) == 0 {
+			return nil
 		}
 
 		f := autobrr.UpdateFilter{Shows: strings.Join(filterTitles, ",")}

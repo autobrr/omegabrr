cdca7952715b33b045fd2af53dfd2dc51a0570c2
ze0s <43699394+zze0s@users.noreply.github.com>
2024-07-21 21:42:26 +0200

    fix(autobrr): host url with multiple slash (#89)

--- pkg/autobrr/client.go
+++ pkg/autobrr/client.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
 	"runtime"
 	"strconv"
 	"time"
@@ -72,14 +73,15 @@ func (c *Client) Test(ctx context.Context) error {
 }
 
 func (c *Client) GetFilters(ctx context.Context) ([]Filter, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.Host+"/api/filters", nil)
+	reqUrl, err := url.JoinPath(c.Host, "/api/filters")
 	if err != nil {
 		return nil, err
 	}
 
-	req.SetBasicAuth(c.BasicUser, c.BasicPass)
-	req.Header.Add("X-API-Token", c.APIKey)
-	c.buildUserAgent(req)
+	req, err := c.newRequest(ctx, http.MethodGet, reqUrl, nil)
+	if err != nil {
+		return nil, err
+	}
 
 	res, err := c.client.Do(req)
 	if err != nil {
@@ -112,14 +114,15 @@ func (c *Client) UpdateFilterByID(ctx context.Context, filterID int, filter Upda
 		return err
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPatch, c.Host+"/api/filters/"+id, bytes.NewBuffer(body))
+	reqUrl, err := url.JoinPath(c.Host, "/api/filters/", id)
 	if err != nil {
 		return err
 	}
 
-	req.SetBasicAuth(c.BasicUser, c.BasicPass)
-	req.Header.Add("X-API-Token", c.APIKey)
-	c.buildUserAgent(req)
+	req, err := c.newRequest(ctx, http.MethodPatch, reqUrl, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
 
 	res, err := c.client.Do(req)
 	if err != nil {
@@ -139,6 +142,19 @@ func (c *Client) UpdateFilterByID(ctx context.Context, filterID int, filter Upda
 	}
 
 	return nil
+}
+
+func (c *Client) newRequest(ctx context.Context, method string, url string, body io.Reader) (*http.Request, error) {
+	req, err := http.NewRequestWithContext(ctx, method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.SetBasicAuth(c.BasicUser, c.BasicPass)
+	req.Header.Add("X-API-Token", c.APIKey)
+	c.buildUserAgent(req)
+
+	return req, nil
 }
 
 func (c *Client) buildUserAgent(req *http.Request) {
